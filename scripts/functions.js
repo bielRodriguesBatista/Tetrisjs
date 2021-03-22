e337032467e678112c0287d47e663a5a43b11fe8
bielRodriguesBatista <Gabriel731Rodrigues@gmail.com>
2021-03-22 20:22:47 -0300

    tetris

--- scripts/functions.js
+++ scripts/functions.js
@@ -5,9 +5,6 @@ function drawSquare(y, x, color){
     // preencher com essa cor 
     tetris.fillRect(y * SQ, x * SQ, SQ, SQ)
 
-
-
-    
     if(color == defaultColor){
         tetris.strokeStyle = defaultBorder
     }
