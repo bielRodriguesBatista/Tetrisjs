f24d33bb3fed4ae7dce4bb536ca68bced0de6230
bielRodriguesBatista <Gabriel731Rodrigues@gmail.com>
2021-03-22 20:30:07 -0300

    upupProjectTetri

--- scripts/functions.js
+++ scripts/functions.js
@@ -5,10 +5,13 @@ function drawSquare(y, x, color){
     // preencher com essa cor 
     tetris.fillRect(y * SQ, x * SQ, SQ, SQ)
 
-    
+
     if(color == defaultColor){
         tetris.strokeStyle = defaultBorder
     }
+
+
+    
 
     // tamanho
     tetris.strokeRect(x * SQ, y * SQ, SQ, SQ);
